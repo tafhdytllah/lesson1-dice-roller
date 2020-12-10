e1b684a1da0f8287a199f695cb867cc951ec2e96
Taufik <tafhdytllah@gmail.com>
2020-12-10 09:45:29 +0700

    First Git

---
+++ app/src/main/java/com/tafh/lesson_1_dice_roller/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.tafh.lesson_1_dice_roller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun rollDice(view: View) {
+        val randomInt = (1 until 6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        tv_result.text = randomInt.toString()
+        iv_dice.setImageResource(drawableResource)
+    }
+}
